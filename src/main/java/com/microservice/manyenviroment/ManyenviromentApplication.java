7effa2f75b47a451751e5a574204a4eec3ecbe16
qjm201000 <1529062902@qq.com>
2018-12-29 10:56:21 +0800

    多环境配置

---
+++ src/main/java/com/microservice/manyenviroment/ManyenviromentApplication.java
@@ -0,0 +1,14 @@
+package com.microservice.manyenviroment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyenviromentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManyenviromentApplication.class, args);
+    }
+
+}
+
